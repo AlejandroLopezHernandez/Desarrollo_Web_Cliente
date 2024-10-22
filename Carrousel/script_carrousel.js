20a657a7f25dddaf16037070ed31da73fff30d48
Alejandro Lopez <alejal07@ucm.es>
2024-10-22 14:05:41 +0200

    subiendo los cambios del carrusel de fotos

---
+++ Carrousel/script_carrousel.js
@@ -0,0 +1,51 @@
+let imagenes = ['anna_karenina.jpeg',
+                'crimen_castigo.png',
+                'guerra_paz.jpeg',
+                'hermanos_kamarazov.jpeg',
+                'maestro_margarita.jpg'];
+
+let imagen = document.querySelector('#imagenes');
+let radios = document.querySelectorAll('input[type="radio"]');
+
+function retroceder(){
+    let seleccionado = null;
+    let imagen_seleccionada = null;
+ 
+    for(let i = 0; i < radios.length; i ++){
+     if (radios[i].checked === true){
+        if(i == 0) {
+            seleccionado = radios[(radios.length) - 1];
+            imagen_seleccionada = imagenes[(imagenes.length) - 1];
+        } else {
+            imagen_seleccionada = imagenes [i - 1];
+            seleccionado = radios[i - 1];
+        }
+     }
+    }
+    imagen.src = imagen_seleccionada;
+    seleccionado.checked = true;
+ }
+
+function avanzar(){
+   let seleccionado = null;
+   let imagen_seleccionada = null;
+
+   for(let i = 0; i < radios.length; i ++){
+    if (radios[i].checked === true){
+        if(i === radios.length - 1){
+            seleccionado = radios[0];
+            imagen_seleccionada = imagenes[0];
+        } else {
+            seleccionado = radios[i + 1];
+            imagen_seleccionada = imagenes [i + 1];
+        }
+        break;
+    }
+   }
+   imagen.src = imagen_seleccionada;
+   seleccionado.checked = true;
+}
+
+
+
+
