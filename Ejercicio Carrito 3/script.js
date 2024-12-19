b64d072f8de7574537e9c615d8f51a2ccb8c1f99
Alejandro Lopez <alejal07@ucm.es>
2024-12-19 14:03:11 +0100

    subiendo el carrito 3

--- Ejercicio Carrito 3/script.js
+++ Ejercicio Carrito 3/script.js
@@ -18,7 +18,7 @@ function AñadirBotonesDesplegables() {
   let contenido = "";
 
   contenido += '<label for="cantidad"> Selecciona los elementos que deseas mostrar: </label>';
-  contenido += '<select name="numeros" id="opciones_numeros">';
+  contenido += '<select name="numeros" id="opciones_numeros" onchange="seleccionarCantidad()">';
   contenido += '<option value="todos">Todos</option>';
   contenido += '<option value="3">3</option>';
   contenido += '<option value="5">5</option>';
@@ -27,7 +27,7 @@ function AñadirBotonesDesplegables() {
   contenido += '<br> <br>'
 
   contenido += '<label for="orden"> Orden de los productos: </label>';
-  contenido += '<select name="numeros" id="opciones_precio">';
+  contenido += '<select name="numeros" id="opciones_precio" onchange="filtrarProductos()">';
   contenido += '<option value="sin_orden">Sin orden</option>';
   contenido += '<option value="mayor">Ordenador por precio mayor</option>';
   contenido += '<option value="menor">Ordenador por precio menor</option>';
@@ -53,35 +53,97 @@ function actualizarCarrito() {
   contador.innerText = carrito.length;
 }
 
-function addElementoDiv() {
+function addElementoDiv(cantidad, filtro) {
   // Seleccionar el primer producto del inventario
   //const producto = inventario[1]; // `carrito` proviene del archivo inventario.js
 
   // Usamos boostrap
   let contenido = "";
-
-  for (producto of inventario) {
-    contenido += '<div class="row">';
-    contenido += '  <div class="col-md-4">';
-    contenido += '    <img src="' + producto['url_imagen'] + '" alt="' + producto['descripcion'] + '" class="img-fluid">';
-    contenido += '  </div>';
-    contenido += '  <div class="col-md-8">';
-    contenido += '    <h5>' + producto['descripcion'] + '</h5>';
-    contenido += '    <p>Precio: $' + producto['precio'] + '</p>';
-    contenido += '    <p>Descuento: ' + producto['descuento'] + '</p>';
-    contenido += '    <button class="btn btn-primary" id="' + producto['id'] + '" >Comprar</button>';
-    contenido += '  </div>';
-    contenido += '</div>';
+  
+  cantidad = document.getElementById('opciones_numeros').value;
+  if(cantidad !== "todos"){
+    cantidad = parseInt(cantidad);
   }
 
+  filtro = document.querySelector('#opciones_precio').value;
+
+  let limite;
+  if (cantidad === "todos"){
+    limite = inventario.length;
+  } else {
+    limite = Math.min(cantidad, inventario.length);
+  }
+
+    for (let i = 0; i < limite; i ++) {
+      contenido += '<div class="row">';
+      contenido += '  <div class="col-md-4">';
+      contenido += '    <img src="' + inventario[i]['url_imagen'] + '" alt="' + inventario[i]['url_imagen'] + '" class="img-fluid">';
+      contenido += '  </div>';
+      contenido += '  <div class="col-md-8">';
+      contenido += '    <h5>' + inventario[i]['descripcion'] + '</h5>';
+      contenido += '    <p>Precio: $' + inventario[i]['precio'] + '</p>';
+      contenido += '    <p>Descuento: ' + inventario[i]['descuento'] + '</p>';
+      contenido += '    <button class="btn btn-primary" >Comprar</button>';
+      contenido += '  </div>';
+      contenido += '</div>';
+    }
 
   // Insertar el contenido generado al contenedor
   productosContainer.innerHTML = contenido;
-
 }
 
+function seleccionarCantidad(){
+  let opcion_cantidad = document.querySelector('#opciones_numeros').value;
 
-addElementoDiv(3);
+  switch(opcion_cantidad){
+    case'todos':
+    addElementoDiv(opcion_cantidad);
+    console.log('prueba todos');
+    break;
+    case '3':
+    addElementoDiv(3);
+    console.log('prueba 3');
+    break;
+    case '5':
+    addElementoDiv(5);
+    console.log('prueba 5');
+    break;
+    case '10':
+    addElementoDiv(10);
+    console.log('prueba 10');
+    break;
+  }
+}
+
+function filtrarProductos(){
+  filtro = document.querySelector('#opciones_precio').value;
+  switch(filtro){
+    case 'sin_orden':
+    addElementoDiv('todos');
+    console.log('prueba sin orden');
+    break;  
+    case 'mayor':
+    
+    inventario.sort((a,b)=> b.precio - a.precio);
+    addElementoDiv('todos','mayor');
+    console.log('prueba de mayor a menor');
+    break;
+    case 'menor':
+    inventario.sort((a,b)=> a.precio - b.precio);
+    addElementoDiv('todos','menor');
+    console.log('prueba de menor a mayor');
+    break;
+
+    case 'descuento':
+    inventario.sort((a,b)=> a.descuento > b.descuento);
+    addElementoDiv('todos','descuento');
+    console.log('prueba orden por descuento');
+    break;
+  }
+}
+
+seleccionarCantidad();
+filtrarProductos();
 // carrito.push(producto);
 // actualizarCarrito()
 // carrito.push(producto);
